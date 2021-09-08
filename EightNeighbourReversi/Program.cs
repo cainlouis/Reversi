baa0d4949de712d4a640b1a93c471121114bbbb8
Nael Louis <nael.louis1002@gmail.com>
2021-09-08 12:02:00 -0400

    first commit - initial setup

---
+++ EightNeighbourReversi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EightNeighbourReversi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
