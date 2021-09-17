d2a88b8fa95c37adef9221da759aea93b13c980c
Nael Louis <nael.louis1002@gmail.com>
2021-09-17 17:09:31 -0400

    Tests done

--- ReversiTest/BoardTest.cs
+++ ReversiTest/BoardTest.cs
@@ -13,6 +13,7 @@ namespace ReversiTest
         [ExpectedException(typeof(IndexOutOfRangeException))]
         public void PlaceRedOutOfBounds_ThrowIndexOutOfRangeException()
         {
+            //Out of bounds
             myDisc = Disc.RED;
             bool result = board.PlaceDisc(3, 4, myDisc);
             Assert.IsFalse(result, "This red disc was able to be placed out of bounds");
@@ -21,7 +22,8 @@ namespace ReversiTest
         [TestMethod]
         public void PlaceRedInlegalPosition_ReturnTrue()
         {
-            myDisc = Disc.WHITE;
+            //Legal Position
+            myDisc = Disc.RED;
             bool result = board.PlaceDisc(1, 0, myDisc);
             Assert.IsTrue(result, "The disc was not able to be in legal position");
         }
@@ -29,6 +31,7 @@ namespace ReversiTest
         [TestMethod]
         public void PlaceDiscInIllegalPosition_ReturnFalse()
         {
+            //No neighbours
             myDisc = Disc.WHITE;
             bool result = board.PlaceDisc(1, 2, myDisc);
             Assert.IsFalse(result, "Was able to place disc in illegal position");
@@ -37,9 +40,19 @@ namespace ReversiTest
         [TestMethod]
         public void PlaceDiscOnTakenPosition_ReturnFalse()
         {
+            //Already taken
             myDisc = Disc.WHITE;
             bool result = board.PlaceDisc(3,0, myDisc);
             Assert.IsFalse(result, "Was able to place disc on a taken position");
+        }
+
+        [TestMethod]
+        public void PlacedWhiteOnRedDisc_ReturnFalse()
+        {
+            //Placed over an Red disc
+            myDisc = Disc.WHITE;
+            bool result = board.PlaceDisc(0, 0, myDisc);
+            Assert.IsFalse(result, "Was able to place disc on opposite disc");
         }
     }
 }
